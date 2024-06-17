b7d76f605e6d48f7e20c21ccd32742d7291fac6f
Joshkriti <Komalkanji13@gmail.com>
2024-06-17 18:47:40 +0100

    First push

--- Restful-Booker/src/test/java/restful/booker/apitest/PostRestfullApi.java
+++ Restful-Booker/src/test/java/restful/booker/apitest/PostRestfullApi.java
@@ -1,8 +1,11 @@
 package restful.booker.apitest;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
+import org.junit.Assert;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
@@ -14,13 +17,59 @@ public class PostRestfullApi {
     ValidatableResponse validatableResponse;
 
     @Test
-    public void createNewBooking(){
+    public void createNewBooking() {
         given()
                 .when()
-                .body("{\"firstname\":\"John\",\"lastname\":\"Smith\",\"totalprice\":\"111\",\"depositpaid\":\"true\",\"checkin\":\"2018-01-01\",\"checkout\":\"2019-01-01\",\"additionalneeds\":\"Breakfast\"}")
+                .body("{\r\n    \"firstname\" : \"Komal\",\r\n    \"lastname\" : \"Kanji\",\r\n    \"totalprice\" : 111,\r\n    \"depositpaid\" : true,\r\n    \"bookingdates\" : {\r\n        \"checkin\" : \"2022-12-01\",\r\n        \"checkout\" : \"2023-10-01\"\r\n    },\r\n    \"additionalneeds\" : \"Full meal\"\r\n}")
+                //.body("{\"firstname\":\"John\",\"lastname\":\"Smith\",\"totalprice\":\"111\",\"depositpaid\":\"true\",\"checkin\":\"2018-01-01\",\"checkout\":\"2019-01-01\",\"additionalneeds\":\"Breakfast\"}")
                 .post("https://restful-booker.herokuapp.com/booking")
                 .then().log().all()
                 .statusCode(200);
+    }
+
+    @Test
+    public void verifyStatusCode(){
+        String bookingData = "{\r\n    \"firstname\" : \"Komal\",\r\n    \"lastname\" : \"Kanji\",\r\n    \"totalprice\" : 111,\r\n    \"depositpaid\" : true,\r\n    \"bookingdates\" : {\r\n        \"checkin\" : \"2022-12-01\",\r\n        \"checkout\" : \"2023-10-01\"\r\n    },\r\n    \"additionalneeds\" : \"Full meal\"\r\n}";
+
+        RestAssured.baseURI = "https://restful-booker.herokuapp.com/booking";
+
+        requestSpecification = RestAssured.given().contentType(ContentType.JSON);
+
+        requestSpecification.body(bookingData);
+
+        response = requestSpecification.post();
+
+        String resString = response.prettyPrint();
+        System.out.println("Response details: " + resString);
+
+        validatableResponse = response.then();
+
+        validatableResponse.statusCode(200);
+
+        validatableResponse.statusLine("HTTP/1.1 200 OK");
+
+    }
+
+    @Test
+    public void verifyUser() {
+
+        Response response = given()
+                .when()
+                .get("https://restful-booker.herokuapp.com/booking/1690");
+
+        String name = response.then().extract().path("firstname");
+        String eName = "Komal";
+
+        Assert.assertEquals(name, eName);
+    }
+
+    @Test
+    public void getEmployeeById() {
+
+        given()
+                .when()
+                .get("https://restful-booker.herokuapp.com/booking/1690")
+                .prettyPrint();
     }
 
 
--- Restful-Booker/src/test/java/restful/booker/apitest/PutRestfullApi.java
+++ Restful-Booker/src/test/java/restful/booker/apitest/PutRestfullApi.java
@@ -1,4 +1,68 @@
 package restful.booker.apitest;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
 public class PutRestfullApi {
+
+    RequestSpecification requestSpecification;
+    ValidatableResponse validatableResponse;
+    Response response;
+
+    @Test
+    public void putNewBooking(){
+        given()
+                .when()
+                .body("{\r\n    \"firstname\" : \"Jiya\",\r\n    \"lastname\" : \"Shah\",\r\n    \"totalprice\" : 111,\r\n    \"depositpaid\" : true,\r\n    \"bookingdates\" : {\r\n        \"checkin\" : \"2022-12-01\",\r\n        \"checkout\" : \"2023-10-01\"\r\n    },\r\n    \"additionalneeds\" : \"Full meal\"\r\n}")
+                .put("https://restful-booker.herokuapp.com/booking/199")
+                .then().log().all()
+                .statusCode(200);
+    }
+
+    @Test
+    public void updateBooking(){
+
+        String updateData = "{\r\n    \"firstname\" : \"Jiya\",\r\n    \"lastname\" : \"Shah\",\r\n    \"totalprice\" : 111,\r\n    \"depositpaid\" : true,\r\n    \"bookingdates\" : {\r\n        \"checkin\" : \"2022-12-01\",\r\n        \"checkout\" : \"2023-10-01\"\r\n    },\r\n    \"additionalneeds\" : \"Full meal\"\r\n}";
+
+        RestAssured.baseURI = "https://restful-booker.herokuapp.com/booking/12";
+
+        requestSpecification = given().contentType(ContentType.JSON);
+
+        //response = requestSpecification.put("key : Cookie , value : token=73670c4cfab7e01, type : text");
+        response = requestSpecification.put();
+        requestSpecification.body(updateData);
+
+        requestSpecification = given().contentType(ContentType.JSON);
+        validatableResponse = response.then();
+
+        // Get status code
+        validatableResponse.statusCode(200);
+
+    }
+
+    @Test
+    public void update(){
+        String updateData = "{\r\n    \"firstname\" : \"Jiya\",\r\n    \"lastname\" : \"Shah\",\r\n    \"totalprice\" : 111,\r\n    \"depositpaid\" : true,\r\n    \"bookingdates\" : {\r\n        \"checkin\" : \"2022-12-01\",\r\n        \"checkout\" : \"2023-10-01\"\r\n    },\r\n    \"additionalneeds\" : \"Full meal\"\r\n}";
+
+        RestAssured.baseURI = "https://restful-booker.herokuapp.com/booking/12";
+
+        requestSpecification = given().contentType(ContentType.JSON);
+        requestSpecification.body(updateData);
+
+        requestSpecification = requestSpecification.header("Cookie"," \"token\": \"a6964e3b9964344\"", "text");
+
+       response = requestSpecification.put();
+
+        validatableResponse = response.then();
+
+        validatableResponse.statusCode(200);
+
+
+    }
 }
